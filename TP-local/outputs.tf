ba37177c77df6d7989335a83bc2724ef17f08aa3
MattheoC-code <mattheo.coppin@ecoles-epsi.net>
2026-03-06 10:22:37 +0100

    feat: exo2

--- TP-local/outputs.tf
+++ TP-local/outputs.tf
@@ -1,4 +1,4 @@
-output "nginx_container_id" {
-  description = "ID of the nginx container"
+output "nginx_container_id" { //doit afficher l’identifiant (id) du conteneur nginx 
+  description = "ID of the nginx docker container"
   value       = docker_container.nginx.id
 }
