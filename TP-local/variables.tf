588ea56d49c68c5254a80a231d01a052d3a38d8d
MattheoC-code <mattheo.coppin@ecoles-epsi.net>
2026-03-24 09:02:52 +0100

    fix: commit test encore oui

--- TP-local/variables.tf
+++ TP-local/variables.tf
@@ -84,7 +84,7 @@ variable "instance_type" {
   description = "Type of EC2 instance"
   type        = string
   default     = "t2.micro"
-}
+}//test
 
 variable "instance_name" {
   description = "Name of the EC2 instance"
